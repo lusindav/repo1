394585cc6be36b22d0dff7c6f2cebe017349d731
lusindav <lusine.davtyan04@mail.ru>
2016-03-03 04:45:03 +0300

    Added file

---
+++ test.java
@@ -0,0 +1,22 @@
+
+public class test {
+	public static void main(String args[]){
+		int i = 3;
+		System.out.println(i);
+	}
+	private static int count(int x){
+		x = x + 1;
+		return x;
+	}
+}
+
+public class test {
+	public static void main(String args[]){
+		int i = 3;
+		System.out.println(i);
+	}
+	private static int count(int x){
+		x = x + 1;
+		return x;
+	}
+}
